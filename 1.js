c9a6c04e175274b626d4ad4f3079cd0f73ba2ea8
terryhung009 <76043300+terryhung009@users.noreply.github.com>
2022-10-07 17:56:37 +0800

    1

--- 1.js
+++ 1.js
@@ -1,10 +1,36 @@
+/* String */
+
 // var age = 30;
 // var str = 'I am a ' + age + ' year-old engineer.';
 // console.log(str);
 
-var age = 30;
-var str = `I am a ${age} year-old engineer.`;
-console.log(str);
+// var age = 30;
+// var str = `I am a ${age} year-old engineer.`;
+// console.log(str);
+
+// var a = 5;
+// var b = 10;
+
+// console.log(`Fifteen is ${a+b} and not ${2 * a + b}.`);
+
+
+/* Number */
+// var a = 10;
+// var b = 12.34;
+
+// console.log(typeof a);
+// console.log(typeof b);
+
+/* Object */
+
+var person = new Object();
+person.name = 'Kuro';
+person.job = 'Front-end developer';
+person.sayName = function(){
+  alert( this.name) ;
+};
+
+person.sayName();
 
 
 
