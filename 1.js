2fa23b963654dbfbb01bcc026efa178b55a9316e
terryhung009 <76043300+terryhung009@users.noreply.github.com>
2022-10-07 17:33:40 +0800

    1

---
+++ 1.js
@@ -0,0 +1,10 @@
+// var age = 30;
+// var str = 'I am a ' + age + ' year-old engineer.';
+// console.log(str);
+
+var age = 30;
+var str = `I am a ${age} year-old engineer.`;
+console.log(str);
+
+
+
